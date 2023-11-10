dbabe41297983c08f5705dd89d1aea01e9c22eef
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-11-11 05:27:30 +0900

    ㅁㅇㄴ

--- src/main/java/com/example/team1_be/utils/security/AuthenticationConfig.java
+++ src/main/java/com/example/team1_be/utils/security/AuthenticationConfig.java
@@ -27,138 +27,141 @@ import lombok.RequiredArgsConstructor;
 @EnableWebSecurity(debug = true)
 @RequiredArgsConstructor
 public class AuthenticationConfig {
-	private final JwtProvider jwtProvider;
-	private final ObjectMapper om;
-	private final Environment env;
+    private final JwtProvider jwtProvider;
+    private final ObjectMapper om;
+    private final Environment env;
 
-	@Value("${cors.origin}")
-	private String CORS_ORIGIN;
+    @Value("${cors.origin}")
+    private String CORS_ORIGIN;
 
-	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.httpBasic()
-			.disable();
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.httpBasic()
+                .disable();
 
-		http.csrf()
-			.disable();
+        http.csrf()
+                .disable();
 
-		applyCorsPolicy(http);
+        applyCorsPolicy(http);
 
-		http.headers()
-			.frameOptions()
-			.disable();
+        http.headers()
+                .frameOptions()
+                .disable();
 
-		http.headers()
-			.xssProtection();
+        http.headers()
+                .xssProtection();
 
-		http.sessionManagement()
-			.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
-		if (isLocalMode()) {
-			authorizeH2Console(http);
-			authorizeApiAndDocs(http);
+        if (isLocalMode()) {
+            authorizeH2Console(http);
+            authorizeApiAndDocs(http);
 
-		} else {
-			http.headers()
-				.contentSecurityPolicy("script-src 'self'");
-		}
+        } else {
+            http.headers()
+                    .contentSecurityPolicy("script-src 'self'");
+        }
 
-		http.exceptionHandling()
-			.authenticationEntryPoint(new CustomAuthenticationEntryPoint(om));
+        http.exceptionHandling()
+                .authenticationEntryPoint(new CustomAuthenticationEntryPoint(om));
 
-		http.exceptionHandling()
-			.accessDeniedHandler(new CustomAccessDeniedHandler(om));
+        http.exceptionHandling()
+                .accessDeniedHandler(new CustomAccessDeniedHandler(om));
 
-		authorizeLogin(http);
+        authorizeLogin(http);
 
-		authorizeGroup(http);
+        authorizeGroup(http);
 
-		authorizeSchedule(http);
+        authorizeSchedule(http);
 
-		authorizeError(http);
+        authorizeError(http);
 
-		http.authorizeHttpRequests()
-			.anyRequest().denyAll();
+        http.authorizeHttpRequests()
+                .anyRequest().denyAll();
 
-		http.addFilterBefore(new CombinedFilter(jwtProvider, om),
-				UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(new JwtAuthenticationFilter(jwtProvider),
+                UsernamePasswordAuthenticationFilter.class);
 
-		return http.build();
-	}
+        http.addFilterBefore(new XSSProtectFilter(om),
+                ChannelProcessingFilter.class);
 
-	private boolean isLocalMode() {
-		String profile = env.getActiveProfiles().length > 0 ? env.getActiveProfiles()[0] : "local";
-		return profile.equals("local");
-	}
+        return http.build();
+    }
 
-	private void applyCorsPolicy(HttpSecurity http) throws Exception {
-		http.cors()
-			.configurationSource(request -> {
-				CorsConfiguration corsConfiguration = new CorsConfiguration();
-				corsConfiguration.setAllowedOrigins(List.of(CORS_ORIGIN));
-				corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-				corsConfiguration.setAllowedHeaders(List.of("*"));
-				corsConfiguration.addExposedHeader("Authorization");
-				return corsConfiguration;
-			});
-	}
+    private boolean isLocalMode() {
+        String profile = env.getActiveProfiles().length > 0 ? env.getActiveProfiles()[0] : "local";
+        return profile.equals("local");
+    }
 
-	private void authorizeError(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests()
-			.antMatchers("/error").permitAll();
-	}
+    private void applyCorsPolicy(HttpSecurity http) throws Exception {
+        http.cors()
+                .configurationSource(request -> {
+                    CorsConfiguration corsConfiguration = new CorsConfiguration();
+                    corsConfiguration.setAllowedOrigins(List.of(CORS_ORIGIN));
+                    corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                    corsConfiguration.setAllowedHeaders(List.of("*"));
+                    corsConfiguration.addExposedHeader("Authorization");
+                    return corsConfiguration;
+                });
+    }
 
-	private void authorizeSchedule(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests()
-			.antMatchers(HttpMethod.GET, "/schedule/application/**")
-			.hasRole(RoleType.ROLE_MEMBER.getAuthority())
-			.antMatchers(HttpMethod.PUT, "/schedule/application")
-			.hasRole(RoleType.ROLE_MEMBER.getAuthority())
-			.antMatchers(HttpMethod.GET, "/schedule/fix/month/**")
-			.hasAnyRole(RoleType.ROLE_ADMIN.getAuthority(), RoleType.ROLE_MEMBER.getAuthority())
-			.antMatchers(HttpMethod.GET, "/schedule/fix/day/**")
-			.hasAnyRole(RoleType.ROLE_ADMIN.getAuthority(), RoleType.ROLE_MEMBER.getAuthority())
-			.antMatchers(HttpMethod.GET, "/schedule/remain/week/**")
-			.hasAnyRole(RoleType.ROLE_ADMIN.getAuthority())
-			.antMatchers(HttpMethod.GET, "/schedule/recommend/**")
-			.hasRole(RoleType.ROLE_ADMIN.getAuthority())
-			.antMatchers(HttpMethod.POST, "/schedule/fix/**")
-			.hasRole(RoleType.ROLE_ADMIN.getAuthority())
-			.antMatchers(HttpMethod.GET, "/schedule/status/**")
-			.hasAnyRole(RoleType.ROLE_ADMIN.getAuthority(), RoleType.ROLE_MEMBER.getAuthority())
-			.antMatchers(HttpMethod.POST, "/schedule/worktime")
-			.hasAnyRole(RoleType.ROLE_ADMIN.getAuthority())
-			.antMatchers(HttpMethod.GET, "/schedule/worktime/**")
-			.hasAnyRole(RoleType.ROLE_ADMIN.getAuthority());
-	}
+    private void authorizeError(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .antMatchers("/error").permitAll();
+    }
 
-	private void authorizeGroup(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests()
-			.antMatchers(HttpMethod.POST, "/group")
-			.hasRole(RoleType.ROLE_ADMIN.getAuthority())
-			.antMatchers(HttpMethod.GET, "/group")
-			.hasAnyRole(RoleType.ROLE_ADMIN.getAuthority(), RoleType.ROLE_MEMBER.getAuthority())
-			.antMatchers(HttpMethod.GET, "/group/invitation")
-			.hasRole(RoleType.ROLE_ADMIN.getAuthority())
-			.antMatchers(HttpMethod.POST, "/group/invitation")
-			.hasRole(RoleType.ROLE_MEMBER.getAuthority())
-			.antMatchers(HttpMethod.GET, "/group/invitation/information/**")
-			.hasRole(RoleType.ROLE_MEMBER.getAuthority());
-	}
+    private void authorizeSchedule(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .antMatchers(HttpMethod.GET, "/schedule/application/**")
+                .hasRole(RoleType.ROLE_MEMBER.getAuthority())
+                .antMatchers(HttpMethod.PUT, "/schedule/application")
+                .hasRole(RoleType.ROLE_MEMBER.getAuthority())
+                .antMatchers(HttpMethod.GET, "/schedule/fix/month/**")
+                .hasAnyRole(RoleType.ROLE_ADMIN.getAuthority(), RoleType.ROLE_MEMBER.getAuthority())
+                .antMatchers(HttpMethod.GET, "/schedule/fix/day/**")
+                .hasAnyRole(RoleType.ROLE_ADMIN.getAuthority(), RoleType.ROLE_MEMBER.getAuthority())
+                .antMatchers(HttpMethod.GET, "/schedule/remain/week/**")
+                .hasAnyRole(RoleType.ROLE_ADMIN.getAuthority())
+                .antMatchers(HttpMethod.GET, "/schedule/recommend/**")
+                .hasRole(RoleType.ROLE_ADMIN.getAuthority())
+                .antMatchers(HttpMethod.POST, "/schedule/fix/**")
+                .hasRole(RoleType.ROLE_ADMIN.getAuthority())
+                .antMatchers(HttpMethod.GET, "/schedule/status/**")
+                .hasAnyRole(RoleType.ROLE_ADMIN.getAuthority(), RoleType.ROLE_MEMBER.getAuthority())
+                .antMatchers(HttpMethod.POST, "/schedule/worktime")
+                .hasAnyRole(RoleType.ROLE_ADMIN.getAuthority())
+                .antMatchers(HttpMethod.GET, "/schedule/worktime/**")
+                .hasAnyRole(RoleType.ROLE_ADMIN.getAuthority());
+    }
 
-	private void authorizeLogin(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests()
-			.antMatchers("/login/kakao").permitAll()
-			.antMatchers("/auth/**").permitAll();
-	}
+    private void authorizeGroup(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .antMatchers(HttpMethod.POST, "/group")
+                .hasRole(RoleType.ROLE_ADMIN.getAuthority())
+                .antMatchers(HttpMethod.GET, "/group")
+                .hasAnyRole(RoleType.ROLE_ADMIN.getAuthority(), RoleType.ROLE_MEMBER.getAuthority())
+                .antMatchers(HttpMethod.GET, "/group/invitation")
+                .hasRole(RoleType.ROLE_ADMIN.getAuthority())
+                .antMatchers(HttpMethod.POST, "/group/invitation")
+                .hasRole(RoleType.ROLE_MEMBER.getAuthority())
+                .antMatchers(HttpMethod.GET, "/group/invitation/information/**")
+                .hasRole(RoleType.ROLE_MEMBER.getAuthority());
+    }
 
-	private void authorizeApiAndDocs(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests()
-			.antMatchers("/api/**", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**").permitAll();
-	}
+    private void authorizeLogin(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .antMatchers("/login/kakao").permitAll()
+                .antMatchers("/auth/**").permitAll();
+    }
 
-	private void authorizeH2Console(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests()
-			.antMatchers("/h2-console/**").permitAll();
-	}
+    private void authorizeApiAndDocs(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .antMatchers("/api/**", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**").permitAll();
+    }
+
+    private void authorizeH2Console(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .antMatchers("/h2-console/**").permitAll();
+    }
 }
