e7b4f0c7e3d481089694985cf3de06df878e0973
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-11-17 02:01:03 +0900

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,10 @@
 insert into `groups` (id, name, phone_number, business_number, address)
-values ('1', '맘스터치', '011-0000-0001', '1', '부산광역시');
+values (1, '맘스터치', '011-0000-0001', 1, '부산광역시');
 
 insert into `users` (id, kakao_id, name, phone_number, is_admin, group_id)
-values ('1', '3040993001', '이재훈', '010-0000-0001', 'true', '1'),
-       ('2', '2', '무지', '010-0000-0007', 'false', '1'),
-       ('3', '3', '라이언', '010-0000-0004', 'false', '1');
+values (1, 3040993001, '이재훈', '010-0000-0001', true, 1),
+       (2, 2, '무지', '010-0000-0007', false, 1),
+       (3, 3, '라이언', '010-0000-0004', false, );
 
 insert into roles (`id`, `role_type`, `user_id`)
 values (1, 'ROLE_ADMIN', 1),
